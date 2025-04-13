4d51f7f74c5535d33ab1761247c8a7a595556692
nibirjoydhar <nibirjoydhar@gmail.com>
2025-04-13 21:44:25 +0600

    CSS updated

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,6 +13,17 @@
             font-size: 20px;
             font-family:'Times New Roman', Times, serif ;
         }
+        a{
+            font-size: 25px;
+            text-decoration: none;
+            background-color: blue;
+            color:white;
+            padding:12px;
+            margin:10px;
+            text-align: center;
+            border-radius:5px;
+
+        }
     </style>
 </head>
 <body>
